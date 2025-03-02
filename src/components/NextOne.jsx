ae846273ab6ce31b1fcc2718d1888bc5c2ad3399
sahatwatz <yoke.21@hotmail.com>
2025-03-03 03:52:43 +0700

    fix button bug

--- src/components/NextOne.jsx
+++ src/components/NextOne.jsx
@@ -5,7 +5,7 @@ export const NextOne = () => {
   return (
     <>
       <div className=" w-auto  flex justify-center mb-5 gap-4">
-        <Link to={"/second-project"}>
+        <Link to={"/first-project"}>
           <a href="">
             <h1 className="text-2xl rounded-full w-fit shadow-xl px-4 p-1  text-primary">
               ←
