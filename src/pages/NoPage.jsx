e9e7037a0adb501eeef6a25be01251b4861b577b
sahatwatz <yoke.21@hotmail.com>
2025-03-03 21:10:58 +0700

    add home button to error page

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ErrorText from "../components/ErrorText";
+import { HomeButton } from "../components/HomeButton";
 
 const NoPage = () => {
   return (
     <>
       <ErrorText />
+      <HomeButton />
     </>
   );
 };
